0d877b796558b608a064767d9da9949a80405016
GrinalRakholiya <grinal.rakholiya@exhibytesolution.com>
2024-03-05 18:32:14 +0530

    get introduction of react js,created react project, learned about components, modules import and export

---
+++ src/Users.js
@@ -0,0 +1,6 @@
+function Users() {
+    return (      
+            <h1>Hello this is Users Page</h1>      
+    )
+}
+export default Users;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Users from './Users';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Users/>
+    <Users/>
+  </React.StrictMode>
+);
+
